35c7f660567c5312c60afd5adfbc52ce23b9d638
Juan Pablo Uriol Balbín <jpuriol@gmail.com>
2021-07-26 12:28:12 +0200

    Created a concurrent simulation. Suprisingly, after running benchmarks it appers the concurrent version is slower than the sequential one.

--- montecarlo.go
+++ montecarlo.go
@@ -1,14 +1,22 @@
 // Package montecarlo provides a simple interface to execute Monte Carlo simulations.
 package montecarlo
 
-const numTrials = 1_000_000
+import (
+	"sync"
+)
+
+const numTrials = 2_000_000
 
 // Returns the probability of the event of interest after running simulations
 //
 // The occurrence of the event of interest is given the return value of the experiment function
 // - true: the event of interest ocurred
-// - false: the event of intereset did NOT ocurred
+// - false: the event of intereset did NOT ocurr
 func Simulation(experiment func() bool) float64 {
+	return simulationSequential(experiment)
+}
+
+func simulationSequential(experiment func() bool) float64 {
 	ocurrencesEvent := 0
 
 	for trial := 0; trial < numTrials; trial++ {
@@ -16,6 +24,43 @@ func Simulation(experiment func() bool) float64 {
 		if eventHappend {
 			ocurrencesEvent++
 		}
+	}
+
+	return float64(ocurrencesEvent) / float64(numTrials)
+}
+
+const nGoroutines = 1
+
+func simulationConcurrent(experiment func() bool) float64 {
+	ch := make(chan int)
+	var wg sync.WaitGroup
+
+	// Launch work in multiple goroutines
+	for i := 0; i < nGoroutines; i++ {
+		wg.Add(1)
+		go func() {
+			localOcurrences := 0
+			for j := 0; j < numTrials/nGoroutines; j++ {
+				eventHappend := experiment()
+				if eventHappend {
+					localOcurrences++
+				}
+			}
+			ch <- localOcurrences
+			wg.Done()
+		}()
+	}
+
+	// Close the channel when all the goroutines are done
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	// Acummulate the results of each experiment
+	ocurrencesEvent := 0
+	for localOcurrences := range ch {
+		ocurrencesEvent += localOcurrences
 	}
 
 	return float64(ocurrencesEvent) / float64(numTrials)
--- montecarlo_test.go
+++ montecarlo_test.go
@@ -27,14 +27,27 @@ func TestDieSimulation(t *testing.T) {
 	actualProbability := Simulation(tossEqualToSix)
 
 	if !compareWithTolerance(expectedProbability, actualProbability) {
-		t.Errorf("expectedProbability=%f and actualProbability=%f => Not same Even by Tolerance\n", expectedProbability, actualProbability)
+		t.Errorf("expectedProbability=%f and actualProbability=%f => Not same by tolerance=%f\n", expectedProbability, actualProbability, tolerance)
 	}
 
 }
 
+func BenchmarkSimulationSequential(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		simulationSequential(tossEqualToSix)
+	}
+}
+
+func BenchmarkSimulationConcurrent(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		simulationConcurrent(tossEqualToSix)
+	}
+}
+
+const tolerance = 0.0001
+
 // Returns true if the numbers are equal with a tolerace, false otherwise
 func compareWithTolerance(a, b float64) bool {
-	tolerance := 0.001
 	if diff := math.Abs(a - b); diff < tolerance {
 		return true
 	}
